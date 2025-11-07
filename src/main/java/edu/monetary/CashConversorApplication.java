dce443a89cd580a3e6a5d1d63cf2c9bf3cac9b44
joaquim <joaquim01001@gmail.com>
2025-11-07 19:29:41 -0300

    criação da estrutura de pastas do Spring.

---
+++ src/main/java/edu/monetary/CashConversorApplication.java
@@ -0,0 +1,13 @@
+package edu.monetary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashConversorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashConversorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/monetary/CashConversorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.monetary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashConversorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
